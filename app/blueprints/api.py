936ed820d7e6e822dff437e81e53f18e7f3d3c59
Parth Acharya <125336330+Projectworkpls@users.noreply.github.com>
2025-07-12 17:39:42 +0530

    Delete app directory

--- app/blueprints/api.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, jsonify
-
-api_bp = Blueprint('api', __name__)
-
-@api_bp.route('/status')
-def status():
-    return jsonify({'status': 'ok'}) 
--- app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .user import User
-from .project import Project
-from .transaction import Transaction
-
-__all__ = ['User', 'Project', 'Transaction'] 
